49a62ca558b58ba4cfe350c52a7af7a2e455744f
root4root <root4root@gmail.com>
2023-07-31 21:21:16 +0300

    Strict EMI protection. Dimmer without delay()

--- demux.cpp
+++ demux.cpp
@@ -1,12 +1,15 @@
 #include <Arduino.h>
 
 #define SERIAL_SPEED 9600
-#define BUTTON_DEBOUNCE_TIMEOUT 250 //milliseconds (150 is enough)
-#define ANTI_EMI 50 //milliseconds (25 is enough)
-#define PRESSED_BUTTON_STATE 1
 
+#define BUTTON_PUSH_DURATION 50    //ms
+#define BUTTON_ACTION_TIMEOUT 100  //ms
+
+#define PRESSED_BUTTON_STATE 1
 #define BUTTON_PIN 6
+
 #define VPNLED_PIN 10
+#define VPNLED_SMOOTH_SPEED 40 //milliseconds
 
 #define CHANNEL1_PIN A3
 #define CHANNEL2_PIN A2
@@ -15,9 +18,10 @@
 
 bool currentOutputState = 0; //readButtonRoutine()
 
-void ledSmoothSwitcher(const bool action);
+void ledSmoothSwitcher(const int8_t = -1);
 void readButtonRoutine();
 void readSerialRoutine();
+//void speedTest();
 
 void setup()
 {
@@ -34,40 +38,71 @@ void loop()
 {
     readButtonRoutine();
     readSerialRoutine();
+    ledSmoothSwitcher();
+    //speedTest(); //72K calls per second ("idle")
 }
 
-void readButtonRoutine()
+
+void readButtonRoutine() //strict EMI protection and debounce
 {
-    static bool previousButtonState = !PRESSED_BUTTON_STATE;
-    static unsigned long previuosChangeTime = 0;
+    static int16_t pushDuration = 0;
+    static uint32_t previousTime = millis(); //in case multiple buttons, could be moved level up
 
-    bool currentButtonState = 0;
+    bool currentButtonState = digitalRead(BUTTON_PIN);
 
-    currentButtonState = digitalRead(BUTTON_PIN);
-
-    if (currentButtonState == previousButtonState) {
+    //unlock pushDuration timer, start action if button pressed
+    if (currentButtonState == PRESSED_BUTTON_STATE && pushDuration == 0) {
+        ++pushDuration;
+        previousTime = millis();
         return;
     }
 
-    //Anti EMI protection (sparking relay etc.)
-    if (millis() - previuosChangeTime > BUTTON_DEBOUNCE_TIMEOUT * 2) {
-    	previuosChangeTime = millis() - (BUTTON_DEBOUNCE_TIMEOUT - ANTI_EMI);
-    	Serial.println("EMI");
-    	return;
-    }
-
-    //Anti debounce protection (bad button's contacts)
-    if (millis() - previuosChangeTime < BUTTON_DEBOUNCE_TIMEOUT) {
+    //idle guard
+    if (pushDuration == 0) {
         return;
     }
 
+    uint16_t timeDifference = millis() - previousTime;
+
+    //minimum time chunk is 1ms
+    if (timeDifference == 0) {
+        return;
+    }
+
+    previousTime = millis();
+
+    //timeout between actions
+    if (pushDuration < 0) {
+        pushDuration += timeDifference;
+
+        //in case heavy duty external operations which takes more than 1ms. Flapping timer protection.
+        if (pushDuration > 0) {
+            pushDuration = 0; //lock pushDuration timer, action completely done
+        }
+
+        //prevent continuously toggling, if button held pressed
+        if (currentButtonState == PRESSED_BUTTON_STATE) {
+            pushDuration = -BUTTON_ACTION_TIMEOUT;
+        }
+
+        return;
+    }
+
+    //EMI protection, including severe ones
     if (currentButtonState == PRESSED_BUTTON_STATE) {
+        pushDuration += timeDifference;
+    } else {
+        pushDuration -= timeDifference;
+    }
+
+    //toggle state, finally...
+    if (pushDuration > BUTTON_PUSH_DURATION) {
         currentOutputState = !currentOutputState;
         digitalWrite(CHANNEL1_PIN, currentOutputState);
+
+        pushDuration = -BUTTON_ACTION_TIMEOUT;
     }
 
-    previousButtonState = currentButtonState;
-    previuosChangeTime = millis();
 }
 
 void readSerialRoutine()
@@ -93,22 +128,54 @@ void readSerialRoutine()
     }
 }
 
-void ledSmoothSwitcher(const bool action)
+void ledSmoothSwitcher(const int8_t action)
 {
-    uint8_t angle = 0;
-    int8_t multiplier = 1;
+    static uint8_t angle = 0;
+    static int8_t step = 1;
+    static unsigned long previuosChangeTime = millis();
+
+    if (angle == 0 && action == -1) {
+        return;
+    }
 
     if (action == 0) {
         angle = 90;
-        multiplier = -1;
+        step = -2;
     }
 
-    for(;;) {
-        angle += multiplier;
-        analogWrite(VPNLED_PIN, 255 * sin(angle * (PI / 180)));
-        if (angle ==  0 || angle == 90) {
-            break;
-        }
-        delay(10); //Fix me
+    if (action == 1) {
+        angle = 1;
+        step = 1;
     }
+
+    if (millis() - previuosChangeTime < VPNLED_SMOOTH_SPEED) {
+            return;
+    }
+
+    angle += step;
+    analogWrite(VPNLED_PIN, 255 * sin(angle * (PI / 180)));
+
+    if (angle == 90) {
+        angle = 0;
+    }
+
+    previuosChangeTime = millis();
 }
+
+/*
+void speedTest()
+{
+    static unsigned long previuosChangeTime = millis();
+    static unsigned long counter = 0;
+
+    if (millis() - previuosChangeTime > 1000) {
+        Serial.println(counter);
+        counter = 0;
+        previuosChangeTime = millis();
+        return;
+    }
+
+    ++counter;
+}
+*/
+
